3c6381a6f19e3baf820cb8f3574543ee333e6eac
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-06-16 19:32:52 +0200

    Updated file header

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Orange Management
+ * Karaka
  *
- * PHP Version 7.4
+ * PHP Version 8.1
  *
  * @package   Modules\Production
  * @copyright Dennis Eichhorn
- * @license   OMS License 1.0
+ * @license   OMS License 2.0
  * @version   1.0.0
  * @link      https://orange-management.org
  */
@@ -23,7 +23,7 @@ use phpOMS\Views\View;
  * Production class.
  *
  * @package Modules\Production
- * @license OMS License 1.0
+ * @license OMS License 2.0
  * @link    https://orange-management.org
  * @since   1.0.0
  */
--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Orange Management
+ * Karaka
  *
- * PHP Version 7.4
+ * PHP Version 8.1
  *
  * @package   Modules\Production
  * @copyright Dennis Eichhorn
- * @license   OMS License 1.0
+ * @license   OMS License 2.0
  * @version   1.0.0
  * @link      https://orange-management.org
  */
@@ -21,7 +21,7 @@ use phpOMS\Module\WebInterface;
  * Production class.
  *
  * @package Modules\Production
- * @license OMS License 1.0
+ * @license OMS License 2.0
  * @link    https://orange-management.org
  * @since   1.0.0
  */
