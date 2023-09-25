fd580bf090721df300b5898e96342532fb5b769d
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 22:54:39 +0000

    fix tests

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -21,7 +21,7 @@ final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'Production';
 
-    protected const URI_LOAD = '';
+    protected const URI_LOAD = 'http://127.0.0.1/en/backend/production';
 
     use \tests\Modules\ModuleTestTrait;
 }
